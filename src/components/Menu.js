644b7f610306b05c6ea57f32b4ba0723eaa3f280
Samuel Ojo <ifegbenga@gmail.com>
2022-02-21 22:23:57 +0100

    data added

---
+++ src/components/Menu.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Styles from "./Menu.module.scss";
+
+const Menu = ({ items }) => {
+  return (
+    <div className={Styles.menu}>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className={Styles["menu__item"]}>
+            <img src={img} alt={title} className={Styles["menu__photo"]} />
+            <div className={Styles["menu__info"]}></div>
+            <header>
+              <h4>{title}</h4>
+              <h4 className={Styles["menu__price"]}>{price}</h4>
+            </header>
+            <p className={Styles["menu__desc"]}>{desc}</p>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
