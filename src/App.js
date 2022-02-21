bccdf011df8a87389cbc75e45c8cc66932d73115
Samuel Ojo <ifegbenga@gmail.com>
2022-02-21 22:22:36 +0100

    changes changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-
+import { useState } from "react";
+import Styles from "./App.module.scss";
+import Categories from "./components/Categories";
+import items from "./components/data";
+import Menu from "./components/Menu";
 
 function App() {
-  return <div></div>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section>
+        <div className={Styles.title}>
+          <h2 className={Styles["title__heading"]}>our menu</h2>
+          <div className={Styles["title__underline"]}></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
