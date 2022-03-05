e49b5ec9b82616c988419e717d2c503d5493cb2f
Samuel Ojo <ifegbenga@gmail.com>
2022-03-06 00:40:26 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+/* eslint-disable */
+
 import { useState } from "react";
 import Styles from "./App.module.scss";
 import Categories from "./components/Categories";
 import items from "./components/data";
 import Menu from "./components/Menu";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -15,7 +28,7 @@ function App() {
           <h2 className={Styles["title__heading"]}>our menu</h2>
           <div className={Styles["title__underline"]}></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import styles from "./Categories.module.scss";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className={styles.btnContainer}>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className={styles.filterB}
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
